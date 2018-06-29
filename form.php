29c9c09fc78035630c1b5579ad1288f9ce710f1d
Demiszcze <diana.serafin83@gmail.com>
2018-06-29 20:09:45 +0200

    form php

--- form.php
+++ form.php
@@ -9,4 +9,15 @@
         mail($to, $subject, $message, $headers);
         echo '<h2>Dziękujemy za wysłanie wiadomości</h2>';
     ?>
+    <form action="form.php" method="POST"> 
+        <label for="name">Imię</label> 
+        <input type="text" name="name" id="name" required/> 
+        <label for="email">E-mail</label> 
+        <input type="email" name="email" id="email" required/> 
+        <label for="message">Wiadomość</label> 
+        <textarea name="message" id="message" rows="3" required/>
+    </textarea> 
+    <input type="submit" name="submit" value="Wyślij"> 
+    <input type="reset" name="submit" value="Usuń dane"> 
+</form>
     
